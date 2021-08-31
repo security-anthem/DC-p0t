0ef73072e9f3177f3772cc0ee964cb4bb8c76d7b
u2i-h1 <73588353+u2i-h1@users.noreply.github.com>
2021-08-31 19:19:23 +0900

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
 ps aux | grep tcpdump | grep -v grep | awk '{ print "kill -INT", $2 }' | sudo sh
-./log/get_log.sh
+ps aux | grep tracer.py | grep -v grep | awk '{ print "kill -INT", $2 }' | sudo sh
+./monitor/get_log.sh
 sudo docker stop victim
 sudo docker rm victim
 echo -e "\n*** DC-p0t stop ***\n"
