e6f9ac751956a601d50be58f41acd5e2cec6f70a
u2i-h1 <73588353+u2i-h1@users.noreply.github.com>
2021-08-31 19:18:29 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash -eu
 
-mkdir -p var/log/
+mkdir -p log/
 python3 setup/create_dockerfile.py
 sudo docker image build -t dc-p0t/victim_con:1 .
 sudo docker run -it -d -p 8080:80 --name victim dc-p0t/victim_con:1
 sudo docker exec victim hostname -i
-rm -f var/log/packet/tcpdump.pcap
-mkdir -p var/log/packet
-sudo tcpdump -i docker0 -s 0 -w var/log/packet/tcpdump.pcap &
+rm -f log/packet/tcpdump.pcap
+mkdir -p log/packet
+sudo tcpdump -i docker0 -s 0 -w log/packet/tcpdump.pcap &
+sudo ./monitor/trace/tracer.py &>> log/ps_monitor.log &
 echo -e "\n\n*** DC-p0t start !! ***\n"
